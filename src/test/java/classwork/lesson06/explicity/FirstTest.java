4ca5f1d35995f09f8a3eda9f0eb3da2c659c3337
Your Name <you@gmail.com>
2018-03-29 19:21:01 +0300

    classwork06

---
+++ src/test/java/classwork/lesson06/explicity/FirstTest.java
@@ -0,0 +1,57 @@
+package classwork.lesson06.explicity;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.core.StringContains.containsString;
+
+public class FirstTest {
+
+    static WebDriver driver;
+    String s = "T-shirt";
+
+    @BeforeClass
+    public static void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+
+        driver.get("http://automationpractice.com/index.php");
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void firstResultContainsText() {
+        driver.findElement(By.id("search_query_top")).click();
+        driver.findElement(By.id("search_query_top")).sendKeys("dress");
+        (new WebDriverWait(driver,10)).until(ExpectedConditions.textToBePresentInElementLocated((By) driver.findElement(By.xpath("//*[@id=\"index\"]/div[2]/ul/li[1]")),"dress"));
+
+        Assert.assertThat(driver.findElement(By.xpath("//*[@id=\"index\"]/div[2]/ul/li[1]")).getText(), containsString("Dress"));
+
+        driver.findElement(By.id("search_query_top")).clear();
+
+        driver.findElement(By.id("search_query_top")).click();
+        driver.findElement(By.id("search_query_top")).sendKeys("T-shirt");
+
+        Assert.assertThat(driver.findElement(By.xpath("//*[@id=\"index\"]/div[2]/ul/li[contains(text(), '" + s + "') and position =1]")).getText(), containsString("t-shirt"));
+
+
+    }
+}
---
+++ src/test/java/classwork/lesson06/imlicity/FirstTest.java
@@ -0,0 +1,44 @@
+package classwork.lesson06.imlicity;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.core.StringContains.containsString;
+
+public class FirstTest {
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+
+        driver.get("http://automationpractice.com/index.php");
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void firstResultContainsText () {
+        driver.findElement(By.id("search_query_top")).click();
+        driver.findElement(By.id("search_query_top")).sendKeys("dress");
+
+        Assert.assertThat(driver.findElement(By.xpath("//*[@id=\"index\"]/div[2]/ul/li[1]")).getText(),containsString("Dress"));
+
+    }
+}
