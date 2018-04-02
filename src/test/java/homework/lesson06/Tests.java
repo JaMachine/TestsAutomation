8187ba4b8d0cfab3d656157001149f9b84cb2235
Auto Tester <Yuriy.Zatonskiy@infopulse.com>
2018-04-02 09:58:00 +0300

    homework 6 done

--- src/test/java/homework/lesson06/Tests.java
+++ src/test/java/homework/lesson06/Tests.java
@@ -10,18 +10,19 @@ import org.openqa.selenium.support.PageFactory;
 
 import static org.hamcrest.core.StringContains.containsString;
 
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Tests extends Base {
     private String log = "e2341745@nwytg.com";
     private String pas = "qwerty";
-    LoginPage pageLog = PageFactory.initElements(d, LoginPage.class);
-    AccountPage pageAcc = PageFactory.initElements(d, AccountPage.class);
+    LoginPage pageLogin = PageFactory.initElements(d, LoginPage.class);
+    AccountPage pageAccount = PageFactory.initElements(d, AccountPage.class);
 
     @Ignore
     @Test
     public void t1_mainLogin() {
 
-        pageAcc = pageLog.logIn(log, pas);
+        pageAccount = pageLogin.logIn(log, pas);
         Assert.assertThat(d.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[2]/a")).getText(),
                 containsString("Sign out"));
     }
@@ -30,9 +31,9 @@ public class Tests extends Base {
     @Test
     public void t2_chainLogin() {
 
-        pageLog.enterUsername(log);
-        pageLog.enterPassword(pas);
-        pageAcc = pageLog.clickSignInBtn();
+        pageLogin.enterUsername(log);
+        pageLogin.enterPassword(pas);
+        pageAccount = pageLogin.clickSignInBtn();
         Assert.assertThat(d.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[2]/a")).getText(),
                 containsString("Sign out"));
     }
@@ -40,7 +41,7 @@ public class Tests extends Base {
     @Test
     public void t3_logOut() {
 
-        pageLog = pageAcc.signOut();
+        pageLogin = pageAccount.signOut();
         Assert.assertThat(d.findElement(By.xpath("//*[@id=\"create-account_form\"]/h3")).getText(),
                 containsString("CREATE AN ACCOUNT"));
     }
