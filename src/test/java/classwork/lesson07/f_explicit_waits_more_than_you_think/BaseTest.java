c7e3f9e5a616869b3c1403d39a04676279cedbd6
Auto Tester <Yuriy.Zatonskiy@infopulse.com>
2018-04-04 17:16:44 +0300

    try new clone

---
+++ src/test/java/classwork/lesson07/f_explicit_waits_more_than_you_think/BaseTest.java
@@ -0,0 +1,21 @@
+package classwork.lesson07.f_explicit_waits_more_than_you_think;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+
+    protected static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        driver.quit();
+    }
+}
